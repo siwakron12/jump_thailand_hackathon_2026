7aa7dc54a4de9efa3f58359293aca4e1794fd0e0
siwakron12 <111340950+siwakron12@users.noreply.github.com>
2026-08-01 18:10:42 +0700

    fix: add listen

--- Backend/my-app/src/index.ts
+++ Backend/my-app/src/index.ts
@@ -4,6 +4,7 @@ import { routes } from "./routes/index"
 const app = new Elysia()
   .get("/", () => "Welcome to Elysia with Better Auth!")
   .use(routes)
-  // .listen(3000)
+ .listen(process.env.PORT || 8080)
+
 
 // console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
