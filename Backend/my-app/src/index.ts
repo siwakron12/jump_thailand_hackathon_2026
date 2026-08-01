e3e4073b8901b39d0bad53f06ab60a65eab67ab9
siwakron12 <111340950+siwakron12@users.noreply.github.com>
2026-08-01 17:33:46 +0700

    be serverless  test depoly

--- Backend/my-app/src/index.ts
+++ Backend/my-app/src/index.ts
@@ -4,6 +4,6 @@ import { routes } from "./routes/index"
 const app = new Elysia()
   .get("/", () => "Welcome to Elysia with Better Auth!")
   .use(routes)
-  .listen(3000)
+  // .listen(3000)
 
-console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
+// console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
