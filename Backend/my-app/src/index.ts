434d6e6a080d96fd7e098ccb1b9ec8ee0639a976
siwakron12 <111340950+siwakron12@users.noreply.github.com>
2026-08-02 12:32:19 +0700

    CORS_ORIGIN

--- Backend/my-app/src/index.ts
+++ Backend/my-app/src/index.ts
@@ -1,10 +1,18 @@
 import { Elysia } from "elysia"
 import { routes } from "./routes/index"
-
+import { cors } from "@elysiajs/cors"
 const app = new Elysia()
+  .use(
+    cors({
+      origin: true, // เปิดรับทุก origin
+      credentials: true, // จำเป็นสำหรับ better-auth เพราะต้องส่ง cookie ข้าม origin
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  )
   .get("/", () => "Welcome to Elysia with Better Auth!")
   .use(routes)
- .listen(process.env.PORT || 8080)
+  .listen(process.env.PORT || 8080)
 
 
 // console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
